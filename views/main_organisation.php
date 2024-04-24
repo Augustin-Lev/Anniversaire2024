0796fe6bd8088c8847ac2a91c7a3aae3ee1df459
Augustin-Lev <augustin.levasseur@etu.unilasalle.fr>
2024-04-24 15:36:41 +0200

    links onedrive

--- views/main_organisation.php
+++ views/main_organisation.php
@@ -10,14 +10,11 @@
     <h1 class="ecriture_claire">Organisation</h1> 
     
     <div class="container btn-container" style="justify-content:center">
-        <a target="_blank" href="https://cloudlasalle-my.sharepoint.com/:x:/g/personal/augustin_levasseur_etu_unilasalle_fr/ERwYUmFlt3lKiOBlce-SjgIB50XMRP9FyV51rTnejuZxWA?e=gbgwLA" class="btn-index fond_clair ecriture_fonce">Liste invité de base</a>
+        <a target="_blank" href="https://cloudlasalle-my.sharepoint.com/:f:/g/personal/augustin_levasseur_etu_unilasalle_fr/EqSNa-dYrSFDnai9qi6sGqYBgiySuAA19gpJ8F1bgqRXcg?e=Cs1QG2" class="btn-index fond_clair ecriture_fonce">OneDrive</a>
         
         <a target="_blank" href="https://www.canva.com/design/DAF_hpN8KYY/K4Tqp8WSloffHLnqiXUI5Q/edit?utm_content=DAF_hpN8KYY&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton" class="btn-index fond_clair ecriture_fonce">Canva</a>
-        
-        <a target="_blank" href="https://cloudlasalle-my.sharepoint.com/:x:/g/personal/augustin_levasseur_etu_unilasalle_fr/EeriWQD22HtOjdos4xLadicBLrsvEBgtGa7i0sJTzGCH8A?e=Dthczl" class="btn-index fond_clair ecriture_fonce">Prévisions Budgetaire</a>
-        
-        <a target="_blank" href="https://cloudlasalle-my.sharepoint.com/:f:/g/personal/augustin_levasseur_etu_unilasalle_fr/EqSNa-dYrSFDnai9qi6sGqYBdBRk4UMI-fX_nKNCimWjwg?e=GbqGWx" class="btn-index fond_clair ecriture_fonce">Musiques localisés</a>
-   
+
+        <a target="_blank" href="https://cloudlasalle-my.sharepoint.com/:x:/g/personal/marie_levasseur_etu_unilasalle_fr/Ebusa5WQMmJAnZlOC_a5-5UBu4mLo1MZf3Lg053NWLRt0w?e=QJED5j" class="btn-index fond_clair ecriture_fonce">Menu</a>
     </div>  
 
     <div class="orgaParent">
